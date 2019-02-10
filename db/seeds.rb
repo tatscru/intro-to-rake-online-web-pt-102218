6ab3b7331fc0d62e69244411ab5487f7390b99f1
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 02:52:55 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@ Student.create(name: "April", grade: "10th")
 Student.create(name: "Luke", grade: "9th")
 Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
+
+
+
+
+# desc 'drop into the Pry console'
+# task :console => :environment do
+#   Pry.start
+# end
